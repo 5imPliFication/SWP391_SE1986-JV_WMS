5a90d82d16b24ef4be89aa5cf42adfe53e845a90
b3rx <hieupg12@gmail.com>
2026-02-03 15:43:48 +0700

    merge iter2

--- Group-3_Warehouse-Management-System/src/main/java/com/example/controller/user/UserCreateServlet.java
+++ Group-3_Warehouse-Management-System/src/main/java/com/example/controller/user/UserCreateServlet.java
@@ -2,6 +2,7 @@ package com.example.controller.user;
 
 import com.example.model.Role;
 import com.example.model.User;
+import com.example.service.RoleService;
 import com.example.service.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,20 +13,25 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static com.example.util.PasswordUtil.hashPassword;
+import java.util.List;
 
 @WebServlet("/user-create")
 public class UserCreateServlet extends HttpServlet {
 
     private UserService userService;
-
+    private RoleService r;
+    
     @Override
     public void init() {
         userService = new UserService();
+        r = new RoleService();
     }
 
     // display screen create new user
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Role> roles = r.getAllRoles();
+        request.setAttribute("roles", roles);
         request.getRequestDispatcher("/WEB-INF/user/user-create.jsp").forward(request, response);
     }
 
