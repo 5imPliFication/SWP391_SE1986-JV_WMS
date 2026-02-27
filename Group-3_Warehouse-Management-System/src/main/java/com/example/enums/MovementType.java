77bf30096c4edd2247dcac26a2b4f79af780e9db
Truong Vu <truongvd0406@gmail.com>
2026-02-27 14:57:03 +0700

    Update .gitignore

---
+++ Group-3_Warehouse-Management-System/src/main/java/com/example/enums/MovementType.java
@@ -0,0 +1,8 @@
+package com.example.enums;
+
+public enum MovementType {
+    IMPORT,
+    EXPORT,
+    ADJUSTMENT,
+    RETURN
+}
---
+++ Group-3_Warehouse-Management-System/src/main/java/com/example/enums/ReferenceType.java
@@ -0,0 +1,7 @@
+package com.example.enums;
+
+public enum ReferenceType {
+    ORDER,
+    GOODS_RECEIPT,
+    AUDIT_ADJUSTMENT
+}
