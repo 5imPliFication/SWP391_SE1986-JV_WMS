c2ade141c976786f34752bf901d24b52d41af5ef
doHung568 <contact.me.dothehung@gmail.com>
2026-03-01 00:16:36 +0700

    feat: create DTO to transfer data for module import history

---
+++ Group-3_Warehouse-Management-System/src/main/java/com/example/dto/GoodsReceiptDTO.java
@@ -0,0 +1,19 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GoodsReceiptDTO {
+    private Long id;
+    private String receiptCode;
+    private Timestamp receivedAt;
+    private String warehouseName;
+    private Long totalQuantity;
+    private String note;
+}
