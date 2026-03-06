72efc6cf1472b5caf41377d9b3c996525f7928c2
b3rx <hieupg12@gmail.com>
2026-03-06 12:41:49 +0700

    change permission checker

--- Group-3_Warehouse-Management-System/src/main/java/com/example/filter/PermissionChecker.java
+++ Group-3_Warehouse-Management-System/src/main/java/com/example/filter/PermissionChecker.java
@@ -8,16 +8,14 @@ public class PermissionChecker {
     private static final Map<String, String> urlPermissionMap = new HashMap<>();
 
     static {
-        urlPermissionMap.put("/user-list", "READ_USER");
-        urlPermissionMap.put("/user-create", "CREATE_USER");
+        urlPermissionMap.put("/user/list", "READ_USER");
+        urlPermissionMap.put("/user/create", "CREATE_USER");
         urlPermissionMap.put("/user", "UPDATE_USER");
-        urlPermissionMap.put("/user-delete", "UPDATE_ROLE");
 
         // --- QUẢN LÝ ROLE ---
         urlPermissionMap.put("/roles", "READ_ROLE");
         urlPermissionMap.put("/create-role", "CREATE_ROLE");
         urlPermissionMap.put("/edit-role", "UPDATE_ROLE");
-        urlPermissionMap.put("/role-delete", "UPDATE_ROLE");
 
         // --- QUẢN LÝ PASSWORD RESET REQUEST ---
         urlPermissionMap.put("/admin/password-reset", "READ_PASSWORD_RESET_REQUEST");
