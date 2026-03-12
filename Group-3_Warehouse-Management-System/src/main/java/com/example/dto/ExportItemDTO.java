cddb3798ed980c2575d1426d529059c38e0475a4
doHung568 <contact.me.dothehung@gmail.com>
2026-03-13 02:55:28 +0700

    fix: change UI and handle logic

---
+++ Group-3_Warehouse-Management-System/src/main/java/com/example/dto/ExportItemDTO.java
@@ -0,0 +1,19 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExportItemDTO {
+    private Long id;
+    private String productName;
+    private int quantity;
+    private BigDecimal priceAtPurchase;
+    private List<ExportProductItemDTO> productItems;
+}
