560db2eed1d6216e087d4bf2d3d51aaba3c04b5a
doHung568 <contact.me.dothehung@gmail.com>
2026-03-23 03:27:06 +0700

    fix: add id for ExportDTO

--- Group-3_Warehouse-Management-System/src/main/java/com/example/dto/ExportDTO.java
+++ Group-3_Warehouse-Management-System/src/main/java/com/example/dto/ExportDTO.java
@@ -13,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ExportDTO {
+    private Long id;
     private String orderCode;
     private String customerName;
     private Double total;
