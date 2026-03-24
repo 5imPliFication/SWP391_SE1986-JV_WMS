9dcc10b34333c54ac8cb84d3b5c820fb6128463e
doHung568 <contact.me.dothehung@gmail.com>
2026-03-24 20:39:23 +0700

    fix: add function save to stock_movements when export

--- Group-3_Warehouse-Management-System/src/main/java/com/example/service/InventoryService.java
+++ Group-3_Warehouse-Management-System/src/main/java/com/example/service/InventoryService.java
@@ -1,10 +1,13 @@
 package com.example.service;
 
 import com.example.dao.InventoryDAO;
+import com.example.dao.StockMovementDAO;
 import com.example.dto.ExportDTO;
 import com.example.dto.ExportProductDTO;
 import com.example.dto.OrderDTO;
 import com.example.dto.ProductItemDTO;
+import com.example.enums.MovementType;
+import com.example.enums.ReferenceType;
 import com.example.model.User;
 import com.example.util.AppConstants;
 import com.example.validator.ImportProductItemValidator;
@@ -199,23 +202,6 @@ public class InventoryService {
         return inventoryDAO.getPendingOrder(orderId);
     }
 
-//    public void completeExportOrder(Long orderId, Long warehouseId) {
-//        // 1. Mark order as completed
-//        inventoryDAO.completeExportOrder(orderId, warehouseId);
-//
-//        // 2. Record stock movement for EXPORT
-//        Map<Long, Long> quantityByProduct = inventoryDAO.getProductQuantityMapForOrder(orderId);
-//
-//        if (!quantityByProduct.isEmpty()) {
-//            new StockMovementDAO().insertStockMovements(
-//                    quantityByProduct,
-//                    MovementType.EXPORT,
-//                    ReferenceType.ORDER,
-//                    orderId
-//            );
-//        }
-//    }
-
 
     public void handleExport(User user, ExportDTO exportOrder, String[] serials) {
         int serialIndex = 0;
@@ -235,6 +221,9 @@ public class InventoryService {
 
         // subtract quantity from inventory
         inventoryDAO.subtractInventory(orderItemSerialsMap);
+
+        // save to stock movement
+        inventoryDAO.saveStockMovements(exportOrder.getId(), orderItemSerialsMap);
 
         // update order status to complete
         inventoryDAO.updateOrderStatus(exportOrder.getId(), user.getId());
